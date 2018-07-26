971353d43d154230d0ec951ea739cd0deb2758e0
Will Schaller <willschaller@gmail.com>
2018-07-26 13:52:26 +0200

    Add headings to app.js

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 'use strict';
 
+// --- Dependencies --- //
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
-const app = express();
-
 const items = require('./routes/items');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+// --- Instantiations --- //
+const app = express();
+
+// --- Configurations --- //
 mongoose.Promise = Promise;
 mongoose.connect('mongodb://localhost/database-name', {
   keepAlive: true,
   reconnectTries: Number.MAX_VALUE
 });
 
+// --- Middleware --- //
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// --- Routes --- //
 app.use('/items', items);
 
-// Error Handling
-// catch 404 and forward to error handler
+// --- Error Handling --- //
 app.use((req, res, next) => {
   res.status(404).json({ code: 'not found' });
 });
 
 app.use((err, req, res, next) => {
-  // always log the error
   console.error('ERROR', req.method, req.path, err);
 
   // only render if the error ocurred before sending the response
