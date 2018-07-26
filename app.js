cd3e0fccad79c69cb16366e16d99144cf0a8ddad
Will Schaller <willschaller@gmail.com>
2018-07-26 12:49:51 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Error Handling
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  res.status(404).json({ code: 'not found' });
+});
+
+app.use((err, req, res, next) => {
+  // always log the error
+  console.error('ERROR', req.method, req.path, err);
+
+  // only render if the error ocurred before sending the response
+  if (!res.headersSent) {
+    res.status(500).json({ code: 'unexpected' });
+  }
+});
+
+module.exports = app;
