8b1c34c61a5bc6f51f8b69339e2230a401baac62
dambusm <willschaller@gmail.com>
2018-10-24 15:39:48 +0200

    Add build script, run with execa

--- routes/builds.js
+++ routes/builds.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const validator = require('validator');
 const execa = require('execa');
 
-router.post('/', (req, res, next) => {});
+router.post('/', (req, res, next) => {
+  const buildScriptPath = './shell/test.sh';
+  execa('sh', [buildScriptPath]).stdout.pipe(process.stdout);
+  res.send('foo');
+});
 
 module.exports = router;
